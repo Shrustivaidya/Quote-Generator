43bd2fa4f94edc8e2c0c56adb3b02d56b433b51e
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2025-03-04 10:51:47 +0530

    Code and implemention

--- quote/src/QuoteGenerator.js
+++ quote/src/QuoteGenerator.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./App.css"; // Import CSS for styling
+import axios from "axios";
 
 const QuoteGenerator = () => {
-  return (
-    <div>
-      <h1>Generator</h1>
-    </div>
-  )
-}
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
 
-export default QuoteGenerator
+  // Function to fetch a random quote
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get("http://api.quotable.io/random");
+      setQuote(response.data.content);
+      setAuthor(response.data.author);
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  };
+
+  // Fetch a quote when the component mounts
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="quote-box">
+        <p className="quote">"{quote}"</p>
+        <h4 className="author">- {author}</h4>
+        <button className="button" onClick={fetchQuote}>
+          Get New Quote
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteGenerator;
